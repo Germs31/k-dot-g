2734558230c15b3fdd8e91bfbac0a3a97a4ae13c
German Nunez <german90nunez@gmail.com>
2020-06-16 16:48:30 -0700

    set up fun fact section and form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Contact = () => {
     return (
         <div className="contact">
-            contact
+            <form>
+                <label>Title:</label>
+                <input type="text" />
+                <label>Name:</label>
+                <input type="text" />
+                <label>Body:</label>
+                <textarea type="text" />
+            </form>
         </div>
     )
 }
--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Faq = () => {
+    const [funFact, setFunFact] = useState([
+        {
+            question: 'How would I describe my friendships?',
+            answer: 'I  would describe my friendships as being solar powered. The brighter and strong my friends shine the more energized I feel. Nothing makes me happier in life than seeing those who I love succeed and be happy'
+        },
+        {
+            question: 'What is my favorite color?',
+            answer: 'My favorite color is black, although technically not a color but the absorption of all the colors. Also love how when exposed to light it gets hot, metaphorically I can relate to rising up to the moment'
+        },
+        {
+            question: 'What do I enjoy most about life',
+            answer:'I enjoy being humbled and not in the sense from being arrogant or pompous but from going to places that leave me astonished or meeting people who have perfected/pushed their craft to new limits. No matter how many times I go to Yosemite National park I will always gaze in astonishment at what nature has created. Just as I will always be amazed by meeting humans who know no boundaries but that of their imagination and hard work.'
+        }
+    ])
+
     return (
-        <div>
+        <div className="faq-section">
+            <div className="fun-fact-container">
+                {
+                    funFact.map(f => f.question)
+                }
+            </div>
+            <div className="fun-btn-container">
+                <button className="fun-btn">
+                    Fun Fact 
+                </button>
+            </div>
         </div>
     )
 }
