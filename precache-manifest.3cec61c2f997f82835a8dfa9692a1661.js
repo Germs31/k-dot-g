6f549ee886d702b3cb109fb5c5bbc21c3bce6c23
German Nunez <german90nunez@gmail.com>
2020-06-16 23:39:27 -0700

    Updates

---
+++ precache-manifest.3cec61c2f997f82835a8dfa9692a1661.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c500373a27430d38afb57f487d29b5dc",
+    "url": "/Germs31/k-dot-g/index.html"
+  },
+  {
+    "revision": "91f2d5f74656064f1449",
+    "url": "/Germs31/k-dot-g/static/css/main.8bcdc5a5.chunk.css"
+  },
+  {
+    "revision": "28f360c9277f05bb5738",
+    "url": "/Germs31/k-dot-g/static/js/2.5f3f34df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Germs31/k-dot-g/static/js/2.5f3f34df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91f2d5f74656064f1449",
+    "url": "/Germs31/k-dot-g/static/js/main.6d475715.chunk.js"
+  },
+  {
+    "revision": "e11fefc761a534173210",
+    "url": "/Germs31/k-dot-g/static/js/runtime-main.bfdc8c96.js"
+  },
+  {
+    "revision": "6769538b501aa8307ed03c3b2de39585",
+    "url": "/Germs31/k-dot-g/static/media/img3.6769538b.jpg"
+  },
+  {
+    "revision": "0fd35326987e56caafbee6b5cdf713c6",
+    "url": "/Germs31/k-dot-g/static/media/landing.0fd35326.jpg"
+  },
+  {
+    "revision": "d2b714a2e13a3984160dc6dbd386df50",
+    "url": "/Germs31/k-dot-g/static/media/resume-bg.d2b714a2.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Germs31/k-dot-g/precache-manifest.3cec61c2f997f82835a8dfa9692a1661.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Germs31/k-dot-g/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
